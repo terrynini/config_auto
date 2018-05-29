b98dc1a1186ddc4b527a2d4a3a3825e7bd62ff8b
terrynini <terrynini38514@gmail.com>
2018-05-29 22:23:34 +0800

    Add:python_package

--- normal_user.sh
+++ normal_user.sh
@@ -1,12 +1,17 @@
 #!/bin/bash                                                                                                
 
-pip install pwntools request pycrypto
+pip install --upgrade pip
+
+python -m pip install pwntools request pycrypto
+python -m pip install ipython
+python3 -m pip install ipython
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/longld/peda.git ~/peda
 
 cp config/zshrc $HOME/.zshrc
 cp config/vimrc $HOME/.vimrc
