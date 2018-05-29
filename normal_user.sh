ed608691fd755bef263fab06685b1d35e0085a36
terrynini <terrynini38514@gmail.com>
2018-05-29 20:41:31 +0800

    Fix:path

--- normal_user.sh
+++ normal_user.sh
@@ -6,8 +6,8 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
  
-cp ./zshrc $HOME/.zshrc
-cp ./vimrc $HOME/.vimrc
+cp config/zshrc $HOME/.zshrc
+cp config/vimrc $HOME/.vimrc
  
 cd ~/
 git clone https://github.com/scwuaptx/Pwngdb.git 
