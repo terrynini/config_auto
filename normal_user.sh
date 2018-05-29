036b25d4efc0791da0c6507121e1e2a72ef9fd1c
terrynini <terrynini38514@gmail.com>
2018-05-29 21:53:55 +0800

    Fix:clone

--- normal_user.sh
+++ normal_user.sh
@@ -5,7 +5,7 @@ pip install pwntools request pycrypto
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
 cp config/zshrc $HOME/.zshrc
