4149380d3581e862e17e682ed3c32bd29f9b9987
terrynini38514 <terrynini38514@gmail.com>
2020-03-24 13:51:04 +0800

    fix

--- package.sh
+++ package.sh
@@ -17,6 +17,7 @@ apt-get install -y python3 python3-dev python3-pip ipython3
 apt-get install -y build-essential fasd
 apt-get install -y libc6-i386 #make ubuntu 64bit able to execut ELF32
 apt-get install -y silversearcher-ag #global
+apt-get install -y libcurl4-openssl-dev
 
 chsh -s /bin/zsh
 
